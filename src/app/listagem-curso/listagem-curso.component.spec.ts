24d318015084c2df80773b8a2795837f9fde25b7
Higor <higor.gsilva1907@gmail.com>
2021-08-18 16:19:16 -0300

    exercicio04

---
+++ src/app/listagem-curso/listagem-curso.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ListagemCursoComponent } from './listagem-curso.component';
+
+describe('ListagemCursoComponent', () => {
+  let component: ListagemCursoComponent;
+  let fixture: ComponentFixture<ListagemCursoComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ListagemCursoComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ListagemCursoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
